824f07d337f168d7edc99e704db984060b1bea4d
Leonardo Pessoa <leonardopessoa799@gmail.com>
2021-12-18 14:12:32 -0300

    modal buttons added

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,8 +1,10 @@
 import closeImg from "../../assets/close.svg";
+import incomeImg from "../../assets/income.svg";
+import outcomeImg from "../../assets/outcome.svg";
 
 import Modal from "react-modal";
 
-import { Container } from "./styles";
+import { Container, TransactionTypeContainer } from "./styles";
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -29,7 +31,21 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
         <h2>Cadastrar Transação</h2>
 
         <input type="text" placeholder="Título" />
+
         <input type="number" placeholder="Valor" />
+
+        <TransactionTypeContainer>
+          <button type="button">
+            <img src={incomeImg} alt="Entrada" />
+            <span>Entrada</span>
+          </button>
+
+          <button type="button">
+            <img src={outcomeImg} alt="Saída" />
+            <span>Saída</span>
+          </button>
+        </TransactionTypeContainer>
+
         <input type="text" placeholder="Categoria" />
 
         <button type="submit">
--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { darken } from 'polished';
 
 export const Container = styled.form`
   h2 {
@@ -49,3 +50,41 @@ export const Container = styled.form`
     }
   }
 `;
+
+export const TransactionTypeContainer = styled.div`
+  margin: 1rem 0;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: .5rem;
+
+  button {
+    height: 4rem;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    border: 1px solid #D7D7D7;
+    border-radius: .25rem;
+    background: transparent;
+
+    transition: border-color .2s;
+
+    &:hover {
+      border-color: ${darken(.1, '#D7D7D7' )};
+    }
+
+    img {
+      width: 20px;
+      height: 20px;
+    }
+
+    span {
+      display: inline-block;
+      margin-left: 1rem;
+
+      font-size: 1rem;
+      color: var(--text-title);
+    }
+  }
+`
